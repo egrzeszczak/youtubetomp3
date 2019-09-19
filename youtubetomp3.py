3508190e5c3ad7ee3d23d17aac5284a16ebd663f
egrzeszczak <ernest.grzeszczak@gmail.com>
2019-09-19 20:01:35 +0200

    Problem with CompSec

--- youtubetomp3.py
+++ youtubetomp3.py
@@ -1,36 +1,32 @@
 from pytube import YouTube, Playlist
-from subprocess import call
+import subprocess
+import ffmpeg
 
 import os
 
+FFMPEG_PATH = r"C:\ffmpeg\bin\ffmpeg.exe"
 current_path = os.getcwd()
 
 def youtubeToMp3(link):
     video = YouTube(link).streams.filter(file_extension='mp4').first()
     print('Downloading {}...'.format(video.default_filename))
 
+    video.download()
     old = os.path.join(os.getcwd(), video.default_filename)
     new = os.path.join(os.getcwd(), os.path.splitext(video.default_filename)[0]) + ".mp3"
 
     print(old + "\n" + new)
 
-    call(["mplayer", "-novideo", "-nocorrect-pts", "-ao", "pcm:waveheader", old])
-    call(["lame", "-v", "audiodump.wav", new])
 
-    # converter = VideoFileClip('test.mp4')
-    # converter.audio.write_audiofile(new)
-    
-    # FFMPEG_VideoReader(old, print_infos=True)
-    # subprocess.Popen(['ffmpeg', '-i', old, new])
-    # converter = ffmpeg.input(old)
-    # converter = ffmpeg.output(converter, new)
-    # ffmpeg.run(converter)
+    # DONT JUDGE ME
+    os.system("""{} -i "{}" "{}" """.format(FFMPEG_PATH, old, new))
+
 
     
 
     
 
-youtubeToMp3("https://www.youtube.com/watch?v=x2tUXts-1wM")
+youtubeToMp3("https://www.youtube.com/watch?v=BettkUR8-5w")
 
 
 
